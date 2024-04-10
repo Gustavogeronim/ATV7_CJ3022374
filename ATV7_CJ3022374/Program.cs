efc2146494f7c098e2f5d37bcf2759dee0ed9516
Gustavogereonim <g.geronimo@aluno.ifsp.edu.br>
2024-04-10 12:34:58 -0300

    qualquer coisa

--- ATV7_CJ3022374/Program.cs
+++ ATV7_CJ3022374/Program.cs
@@ -1,137 +1,197 @@
-﻿namespace ATV7_CJ3022374
+﻿int opcao;
+
+do
 {
-    internal class Program
+    do
     {
-        static void Main(string[] args)
-        {
-            int opcao;
+
+        Console.WriteLine("=========MENU=========");
+        Console.WriteLine("0 - SAIR");
+        Console.WriteLine("1 - EX1");
+        Console.WriteLine("2 - EX2");
+        Console.WriteLine("3 - EX3");
+        Console.WriteLine("4 - EX4");
+        Console.WriteLine("5 - EX5");
+        Console.WriteLine("6 - EX6");
+        Console.WriteLine("7 - EX7");
+
+        opcao = int.Parse(Console.ReadLine());
+
+    } while (opcao < 0 || opcao > 7);
+
+    switch (opcao)
+    {
+        case 0:
+            Console.WriteLine("Obrigado por usar o sistema!");
+            break;
+
+
+        //Exercício 1
+
+        case 1:
+
+            int n = 1, i;
+
+            Console.WriteLine("Digite um valor");
+            i = int.Parse(Console.ReadLine());
 
             do
             {
-                do
+
+                Console.WriteLine(n);
+                n++;
+            } while (n >= i);
+            break;
+
+        //Exercício 2
+
+        case 2:
+
+            int n1 = 1, i1;
+
+            Console.WriteLine("Digite um valor");
+            i1 = int.Parse(Console.ReadLine());
+
+            do
+            {
+
+                if (n1 % 2 == 0)
                 {
 
-                    Console.WriteLine("=========MENU=========");
-                    Console.WriteLine("0 - SAIR");
-                    Console.WriteLine("1 - EX1");
-                    Console.WriteLine("2 - EX2");
-                    Console.WriteLine("3 - EX3");
+                    Console.WriteLine(n1);
+                    n1++;
+                }
+            } while (n1 <= i1);
+            break;
 
-                    opcao = int.Parse(Console.ReadLine());
+        case 3:
 
-                } while (opcao < 0 || opcao > 3);
+            int d = 1000, c;
 
-                switch (opcao)
+            Console.WriteLine("Digite um valor menor que 1000");
+            c = int.Parse(Console.ReadLine());
+
+            do
+            {
+
+                if (d % 2 == 0)
                 {
-                    case 0:
-                        Console.WriteLine("Obrigado por usar o sistema!");
-                        break;
+
+                    Console.WriteLine(d);
+                    d--;
+                }
+            } while (d > c);
+
+            break;
 
 
-                    case 1:
-                        Console.WriteLine("Executando o ex1");
-
-                        // Atividade 1
-                        int n = 1, i;
-
-                        Console.WriteLine("Digite um número:");
-                        i = int.Parse(Console.ReadLine());
+        case 4:
 
 
-                        do
-                        {
-                            Console.WriteLine(n);
-                            n++;
-                        }
-                        while (n <= i);
-                        break;
-
-                    case 2:
-                        Console.WriteLine("Executando o ex2");
-                        int r = 1, j;
-
-                        Console.WriteLine("Digite um número:");
-                        j = int.Parse(Console.ReadLine());
-
-                        do
-                        {
-                            if (r == 1 || r % 2 == 0)
-                                Console.WriteLine(r);
-                            r++;
-                        }
-                        while (r <= j);
-
-                        break;
+            int k;
+            int s = 0;
 
 
-                    case 3:
-                        Console.WriteLine("Executando o ex3");
-                        int p = 1000, l;
+            do
+            {
+                Console.WriteLine("Digite um valor: ");
+                k = int.Parse(Console.ReadLine());
 
-                        Console.WriteLine("Digite um número até 1000:");
-                        l = int.Parse(Console.ReadLine());
+                if (k > 0)
+                {
+                    s = s + k;
 
-
-                        if (p <= 1000)
-                        {
-                            do
-                            {
-                                if (p % 2 == 0)
-                                    Console.WriteLine(p);
-                                p--;
-
-                            } while (p <= l);
-                        }
-                        else
-                        {
-                            Console.WriteLine("O número é maior que mil");
-                        }
-
-                        break;
-
-
-                    case 4:
-                        Console.WriteLine("Executando o ex3");
-
-                        int valor = 0, n1, conta = 0;
-
-                        do
-                        {
-
-                            Console.WriteLine("Digite um número");
-                            n1 = int.Parse(Console.ReadLine());
-
-                            valor += n1;
-                            conta++;
-
-
-
-                            if (n1 >= 0)
-                            {
-                                Console.WriteLine("O útimo número positivo é:{0}", n1);
-                                Console.WriteLine("A soma dos números até agora é de", valor);
-
-                            }
-                            else if (n1 < 0)
-
-                            {
-                                Console.WriteLine("útimo número negativo digitado é:", n1);
-                                Console.WriteLine("a soma dos números até agora é:", valor);
-                            }
-                            if (valor >= 200)
-                            {
-                                break;
-                            }
-                        } while (true);
-                        break;
-
-                    case 5:
-                        Console.WriteLine("Executando o ex3");
-                        break;
-
+                    Console.WriteLine("Soma = {0}", s);
                 }
 
-            } while (opcao != 0);
-        }
+
+
+            } while (s < 200);
+
+            break;
+
+        case 5:
+
+            int div, a1 = 0;
+
+            Console.WriteLine("Digite um número inteiro: ");
+            div = int.Parse(Console.ReadLine());
+
+            do
+            {
+                if (div % a1 == 0)
+                {
+                    Console.WriteLine(a1);
+                    a1++;
+                }
+            } while (a1 <= div);
+
+
+            break;
+
+
+        case 6:
+
+
+            int j = 0, v;
+
+            do
+            {
+                Console.WriteLine("Digite um valor");
+                v = int.Parse(Console.ReadLine());
+                j++;
+
+                if (v < v)
+                {
+                    Console.WriteLine("Número maior= {0}", v);
+                }
+                if (v > v)
+                {
+                    Console.WriteLine("Número menor= {0}", v);
+                }
+
+
+            } while (j < 11);
+
+            break;
+        case 7:
+
+            int u;
+            int si = 0;
+            int sp = 0;
+
+            Console.WriteLine("Digite um valor: ");
+            u = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Digite um valor: ");
+
+                u = int.Parse(Console.ReadLine());
+
+
+                if (u % 2 != 0)
+                {
+                    si = si + u;
+                }
+                if (u % 2 == 0)
+                {
+                    sp = sp + u;
+                }
+
+                Console.WriteLine("Soma dos números Ímpares= {0}, ", si, "Soma dos números Pares= {1} ", sp);
+            } while (u != 0);
+
+
+
+                    break;
+
+
     }
-}
+
+} while (opcao != 0);
+            
+// feliz
+    
+
+        
